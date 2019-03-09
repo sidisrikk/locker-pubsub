3a2c6b8d24af42ff86a344381bc0f0f2bf474092
sidi <s>
2019-03-10 06:04:19 +0700

    connect db + loadfixture script

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs, resolvers } from './graphql';
+import { sequelize } from './models/connect';
 
 const server = new ApolloServer({ typeDefs, resolvers });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
---
+++ src/models/connect.js
@@ -0,0 +1,42 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('sqlite:database.sqlite');
+
+// define Models
+const Unit = sequelize.define('unit', {
+  'passcode': {
+    'type': Sequelize.STRING,
+  },
+  'no': {
+    'type': Sequelize.STRING,
+    'primaryKey': true,
+  },
+  'status': {
+    'type': Sequelize.ENUM('available', 'reserved'),
+  },
+  'firstPricePerHour': {
+    'type': Sequelize.INTEGER,
+  },
+  'laterPricePerHour': {
+    'type': Sequelize.INTEGER,
+  },
+});
+const Locker = sequelize.define('locker', {
+  'no': {
+    'type': Sequelize.STRING,
+    'primaryKey': true,
+  },
+});
+Locker.hasMany(Unit);
+
+sequelize.sync({ 'logging': false }).then(() => {
+  console.log('sync ok.');
+}).catch((err) => {
+  console.log(`sync fail : ${err}`);
+});
+
+export {
+  sequelize,
+  Unit,
+  Locker,
+};
