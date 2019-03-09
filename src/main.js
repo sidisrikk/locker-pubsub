b9cb50df8a217e13e1d637c9e41ba023ec67135d
sidi <s>
2019-03-09 23:07:28 +0700

    rm comment

--- src/main.js
+++ src/main.js
@@ -28,8 +28,6 @@ const typeDefs = gql`
   }
 `;
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
     test: () => 'ok',
@@ -49,13 +47,8 @@ const resolvers = {
   },
 };
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
